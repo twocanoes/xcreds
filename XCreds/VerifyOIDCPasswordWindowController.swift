d8ace707133dde734c5c17c12e2427f46f1ef8e5
tperfitt <tperfitt@twocanoes.com>
2022-06-04 12:37:18 -0500

    added window controllers and xibs

---
+++ XCreds/VerifyOIDCPasswordWindowController.swift
@@ -0,0 +1,33 @@
+//
+//  VerifyOIDCPassword.swift
+//  XCreds
+//
+//  Created by Timothy Perfitt on 6/4/22.
+//
+
+import Cocoa
+
+class VerifyOIDCPassword: NSWindowController {
+
+    @IBOutlet weak var passwordTextField: NSSecureTextField!
+
+    var password:String?
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+    }
+    
+    @IBAction func updateButtonPressed(_ sender: Any) {
+        if self.window?.isModalPanel==true {
+            password=passwordTextField.stringValue
+            NSApp.stopModal(withCode: .OK)
+
+        }
+    }
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        if self.window?.isModalPanel==true {
+            NSApp.stopModal(withCode: .cancel)
+            self.window?.close()
+        }
+    }
+}
