eacff8d96ebfa857048f5dbc0d2096435620acc7
tperfitt <tperfitt@twocanoes.com>
2022-06-04 17:00:12 -0500

    added immediate check after launch

--- XCreds/ScheduleManager.swift
+++ XCreds/ScheduleManager.swift
@@ -13,41 +13,18 @@ class ScheduleManager {
 
     var timer:Timer?
     func startCredentialCheck()  {
-
         if let timer = timer, timer.isValid==true {
             return
         }
-
         var rate = UserDefaults.standard.integer(forKey: PrefKeys.refreshRate.rawValue)
 
         if rate < 5 {
             rate = 5
         }
         timer=Timer.scheduledTimer(withTimeInterval: TimeInterval(rate), repeats: true, block: { timer in
-            TokenManager.shared.getNewAccessToken(completion: { isSuccessful, hadConnectionError in
-
-                if hadConnectionError==true {
-                    NotifyManager.shared.sendMessage(message: "Could not check token.")
-
-                    return
-                }
-                else if isSuccessful == true {
-                    NotifyManager.shared.sendMessage(message: "Azure password unchanged")
-
-                }
-                else {
-                    timer.invalidate()
-                    NotifyManager.shared.sendMessage(message: "Azure password changed or not set")
-                    DispatchQueue.main.async {
-                        mainMenu.webView = WebViewController()
-                        mainMenu.webView?.window!.forceToFrontAndFocus(nil)
-                        mainMenu.webView?.run()
-
-                    }
-
-                }
-            })
+            self.checkToken()
         })
+        self.checkToken()
     }
     func stopCredentialCheck()  {
         if let timer = timer, timer.isValid==true {
@@ -55,4 +32,33 @@ class ScheduleManager {
 
         }
     }
+    func checkToken()  {
+        TokenManager.shared.getNewAccessToken(completion: { isSuccessful, hadConnectionError in
+
+            if hadConnectionError==true {
+                NotifyManager.shared.sendMessage(message: "Could not check token.")
+
+                return
+            }
+            else if isSuccessful == true {
+                NotifyManager.shared.sendMessage(message: "Azure password unchanged")
+
+            }
+            else {
+                if let timer = self.timer {
+                    timer.invalidate()
+                }
+                NotifyManager.shared.sendMessage(message: "Azure password changed or not set")
+                DispatchQueue.main.async {
+                    mainMenu.webView = WebViewController()
+                    mainMenu.webView?.window!.forceToFrontAndFocus(nil)
+                    mainMenu.webView?.run()
+
+                }
+
+            }
+        })
+
+    }
+
 }
