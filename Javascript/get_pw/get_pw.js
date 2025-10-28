91a9e47fddef2ece7f1855f727d4f2793f5ab78c
Tmothy Perfitt <tperfitt@twocanoes.com>
2025-10-28 16:27:51 -0500

    updated js

--- Javascript/get_pw/get_pw.js
+++ Javascript/get_pw/get_pw.js
@@ -1,16 +1,32 @@
-var elementValues = [];
-var elements = [];
+var elements = new Set();
 var result = {
   "passwords": [],
   "ids": [],
 };
-window.addEventListener('input', function(){
-  elements = document.querySelectorAll('[type="password"]');
 
-  for (let i = 0; i < elements.length; i++) {
-    elements[i].addEventListener('input', function(){
-      result.passwords = Array.from(elements).map(i=>i.value);
-      result.ids = Array.from(elements).map(i=>i.id);
-    });
-  };
-});
+function watchWindow(){
+  var passwordElements = document.querySelectorAll('[type="password"]');
+
+  if (passwordElements.length > 0) {
+    elements.add(...passwordElements);
+  }
+
+  var elementsArray = Array.from(elements);
+  elementsArray.filter(function(i){
+    return i.value !== "";
+  });
+
+  if (elementsArray.length == 0) {
+    console.log("No password fields found");
+  }
+
+  result.passwords = elementsArray.map(i=>i.value)
+    .filter(i=>i !== "");
+  result.ids = elementsArray.map(i=>i.id);
+  result.ids = [...new Set(result.ids)];
+  console.log(result);
+}
+
+watchWindow();
+window.addEventListener('click', watchWindow);
+window.addEventListener('input', watchWindow);
