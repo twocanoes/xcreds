b23f569ab769a07e1f9c757d4e1351266b37a3d0
Tmothy Perfitt <tperfitt@twocanoes.com>
2026-02-20 14:20:14 -0600

    fixed crash when dragging

--- XCredsLoginPlugIn/LoginWindow/MainLoginWindow.swift
+++ XCredsLoginPlugIn/LoginWindow/MainLoginWindow.swift
@@ -11,4 +11,10 @@ class MainLoginWindow: NSWindow {
     override var canBecomeKey: Bool {
         return true
     }
+    
+    //dragging cause crash. don't care about drag support so override
+    //with empty
+    override func drag(_ image: NSImage, at baseLocation: NSPoint, offset initialOffset: NSSize, event: NSEvent, pasteboard pboard: NSPasteboard, source sourceObj: Any, slideBack slideFlag: Bool) {
+        return
+    }
 }
